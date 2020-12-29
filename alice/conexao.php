35c17e991f2edab1b185bcd0f2e325b428eb54ea
Heloisa Gutierrez Sabioni <heloguti64@gmail.com>
2020-12-29 18:18:36 -0300

    corrigindo

--- alice/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-$stringdeconexao = "host=localhost port=5432 dbname=b31sayuridottore user=b31sayuridottore password=sayuri123";
-
-$conecta = pg_connect($stringdeconexao);
-
-if (!$conecta) {   
-    exit;
-}
-?>    
